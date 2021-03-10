3f4b3547929eeb940c08adf819c89da7898f7e71
FarzamAlam <farzamcse@gmail.com>
2021-03-10 17:19:32 +0530

    Added docker file

---
+++ src/main/java/org/mycompany/models/Request.java
@@ -0,0 +1,15 @@
+package org.mycompany.models;
+
+public class Request {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
